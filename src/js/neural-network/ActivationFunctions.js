48805b19cd9ec81897f9e3e924c8de3fd30d1ea6
Christian Stussak <stussak@mfo.de>
2020-10-06 11:31:31 +0200

    Add 'binary' and 'sigmoid' activation functions

--- src/js/neural-network/ActivationFunctions.js
+++ src/js/neural-network/ActivationFunctions.js
@@ -1,7 +1,17 @@
+const binary = { f: z => z < 0 ? 0 : 1, dF: z => 0 };
 const linear = { f: z => z, dF: _ => 1 };
 const relu = { f: z => Math.max(0, z), dF: z => z < 0 ? 0 : 1 };
+const sigmoid = {
+  f: z => 1 / (1 + Math.exp(-z)),
+  dF: z => {
+    const e = Math.exp(-z);
+    return e / ((1 + e) * (1 + e));
+  }
+};
 
 export {
+  binary,
   linear,
   relu,
+  sigmoid,
 };
