fe2d3e4069f6a0899eb974ffdabca5db6532dc3c
Christian Stussak <stussak@mfo.de>
2020-10-22 10:20:25 +0200

    Allow clamping of input values to values in array instead of range

--- src/js/neural-network/Model.js
+++ src/js/neural-network/Model.js
@@ -222,11 +222,26 @@ function assignUndefinedCloneDeep(target, ...sources) {
 /***
  *
  * @param value {number}
- * @param range {Interval}
+ * @param range {Interval|Number[]}
  * @returns {number}
  */
 function clamp(value, range) {
-  return Math.min(Math.max(range.lo, value), range.hi);
+  if (IOps.isInterval(range)) {
+    return Math.min(Math.max(range.lo, value), range.hi);
+  } else if (Array.isArray(range) && range.length > 0) {
+    let closest = range[0];
+    let closestDist = Math.abs(value - range[0]);
+    for (let i = 1; i < range.length; ++i) {
+      const dist = Math.abs(value - range[i]);
+      if (dist < closestDist) {
+        closest = range[i];
+        closestDist = dist;
+      }
+    }
+    return closest;
+  } else {
+    throw Error(`Range must be an interval or non-empty array. Got ${range} instead.`);
+  }
 }
 
 Object.defineProperty(
