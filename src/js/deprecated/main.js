396cfa0bdb8308a287cfef2ed6af253bcf5620d9
Christian Stussak <stussak@mfo.de>
2020-10-28 16:30:22 +0100

    Move deprecated files out of the way

---
+++ src/js/deprecated/main.js
@@ -0,0 +1,35 @@
+import { LevelController } from './LevelController';
+
+function main() {
+  new LevelController();
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector("#helpmebutton").onclick = () => {
+      document.querySelector(".helper").classList.add("visible");
+      document.querySelector(".mission").classList.remove("visible");
+      document.querySelector("#helpmebutton").classList.add("selected");
+      document.querySelector("#missionbutton").classList.remove("selected");
+    };
+
+    document.querySelector("#missionbutton").onclick = () => {
+      document.querySelector(".helper").classList.remove("visible");
+      document.querySelector(".mission").classList.add("visible");
+      document.querySelector("#helpmebutton").classList.remove("selected");
+      document.querySelector("#missionbutton").classList.add("selected");
+    };
+    /* FIXME: There is no #creditsbutton linear, so why is this code here if it breaks anyway?
+      document.querySelector("#creditsbutton").onclick = () => {
+        document.querySelector(".credits").classList.toggle("visible");
+        document.querySelector("#screenoverlay").classList.toggle("visible");
+        document.querySelector("#creditsbutton").classList.toggle("selected");
+      };
+    */
+    document.querySelector("#screenoverlay").onclick = () => {
+      document.querySelector(".credits").classList.remove("visible");
+      document.querySelector("#screenoverlay").classList.remove("visible");
+      document.querySelector("#creditsbutton").classList.remove("selected");
+    };
+  });
+}
+
+main();
--- src/js/main.js
+++ src/js/main.js
@@ -2,44 +2,11 @@ import View from './neural-network-mvc/view';
 import Controller from './neural-network-mvc/controller';
 import { load as loadLevel } from './level/load';
 
-function main() {
-  //new LevelController();
-
-
-  window.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector("#helpmebutton").onclick = () => {
-      document.querySelector(".helper").classList.add("visible");
-      document.querySelector(".mission").classList.remove("visible");
-      document.querySelector("#helpmebutton").classList.add("selected");
-      document.querySelector("#missionbutton").classList.remove("selected");
-    };
-
-    document.querySelector("#missionbutton").onclick = () => {
-      document.querySelector(".helper").classList.remove("visible");
-      document.querySelector(".mission").classList.add("visible");
-      document.querySelector("#helpmebutton").classList.remove("selected");
-      document.querySelector("#missionbutton").classList.add("selected");
-    };
-    /* FIXME: There is no #creditsbutton linear, so why is this code here if it breaks anyway?
-      document.querySelector("#creditsbutton").onclick = () => {
-        document.querySelector(".credits").classList.toggle("visible");
-        document.querySelector("#screenoverlay").classList.toggle("visible");
-        document.querySelector("#creditsbutton").classList.toggle("selected");
-      };
-    */
-    document.querySelector("#screenoverlay").onclick = () => {
-      document.querySelector(".credits").classList.remove("visible");
-      document.querySelector("#screenoverlay").classList.remove("visible");
-      document.querySelector("#creditsbutton").classList.remove("selected");
-    };
-  });
-}
-
 function rand(min, max) {
   return Math.random() * (max - min) - min;
 }
 
-async function mainMaxModel() {
+async function main() {
   const levelUrl = new URL('assets/levels/Max.yaml', window.location.href);
   const { model, layout, training, strings } = await loadLevel(levelUrl);
   const network = model.network;
@@ -65,5 +32,4 @@ async function mainMaxModel() {
   const controller = new Controller(model, training.targetActivationFuncs, view);
 }
 
-mainMaxModel();
-//main();
+main();
