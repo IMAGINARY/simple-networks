4b0e247054b57fc1304226ac648f471bfc66c3df
Christian Stussak <stussak@mfo.de>
2020-11-13 10:59:08 +0100

    Fix handling of unsuccesful fetches in fetchWithCache

--- src/js/util/fetch-with-cache.js
+++ src/js/util/fetch-with-cache.js
@@ -2,12 +2,11 @@ const cache = new Map();
 
 export default async function fetchWithCache(href) {
   if (cache.has(href)) {
-    try {
-      return (await cache.get(href)).clone();
-    } catch (e) {
+    const response = await cache.get(href);
+    if (!response.ok) {
       cache.delete(href);
-      throw e;
     }
+    return response.clone();
   } else {
     const fetcher = fetch(href);
     cache.set(href, fetcher);
